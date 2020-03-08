7a859dca2800c2079a9295d8f0821fcfe612df3e
neerajdana <neerajdana9@gmail.com>
2020-03-08 22:36:39 +0530

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,30 @@
 module.exports = {
-  apps: [
-    {
-      name: "smart-analytics-api",
-      script: "./dist/smartanalyticapi.js",
-      env: {
-        NODE_ENV: "development"
-      },
-      env_production: {
-        NODE_ENV: "production"
-      },
-      watch: true,
-      error_file: "err.log",
-      out_file: "out.log",
-      log_file: "combined.log"
+  apps : [{
+    name: 'SMART_ANALYTIC_API',
+    script: 'smartanalyticapi.js',
+
+    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+   
+    instances: 1,
+    autorestart: true,
+    watch: true,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
     }
-  ]
+  }],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
 };
